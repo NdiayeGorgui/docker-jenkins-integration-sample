3d5db81e4863ecd418cabc795a446f25a22d78e4
Gorgui Ndongo <gorguindongondiaye@gmail.com>
2022-07-16 18:15:11 -0400

    first commit

---
+++ src/main/java/com/srv/springbootdockerintegration/SpringBootJenkinsDockerIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.srv.springbootdockerintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsDockerIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsDockerIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srv/springbootdockerintegration/SpringBootJenkinsDockerIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srv.springbootdockerintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsDockerIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
