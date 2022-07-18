d392373cb48976c34b4c94c0d26ca757f0465b0e
NdiayeGorgui <90470613+NdiayeGorgui@users.noreply.github.com>
2022-07-17 22:10:20 -0400

    Update SpringBootJenkinsDockerIntegrationApplication.java

--- src/main/java/com/srv/springbootdockerintegration/SpringBootJenkinsDockerIntegrationApplication.java
+++ src/main/java/com/srv/springbootdockerintegration/SpringBootJenkinsDockerIntegrationApplication.java
@@ -2,9 +2,16 @@ package com.srv.springbootdockerintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootJenkinsDockerIntegrationApplication {
+	@GetMapping("/test")
+	public String getMessage() {
+		return "demo jenkins";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJenkinsDockerIntegrationApplication.class, args);
